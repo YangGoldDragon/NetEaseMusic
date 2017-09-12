7a4f526e3479eb3d254ab8877867c569401d7719
yangjinlong <835383258@qq.com>
2017-09-12 11:32:02 +0800

    完成songs.json数据库信息

--- src/js/song.js
+++ src/js/song.js
@@ -2,8 +2,52 @@
  * Created by 金龙 on 2017/9/5.
  */
 $(function () {
+    let id = location.search.match(/\bid=([^&]*)/)[1];
+    $.get('./songs.json').then(function (response) {
+        let songs = response;
+        if(typeof songs === 'string'){
+            songs = songs.split('/^},/');
+        }
+        let song = songs.filter((s)=>{return s.id === id})[0];
+        let {url} = song;
+        let audio = document.createElement('audio');
+        audio.src = url;
+        audio.oncanplay = function () {
+            audio.play();  //出于保护用户的目的，在移动端禁止加载资源之后自动播放，巨坑。该属性只能在PC端有效，移动端无效。
+        };
+        $('.icon-pause').on('click',function () {
+            audio.pause();
+            $(this).removeClass('icon-show');
+            $('.icon-play').addClass('icon-show');
+            $('.disc-img2').css({
+                'animation-play-state': 'paused',
+                '-webkit-animation-play-state': 'paused'
+            });
+            $('.disc-img3').css({
+                'animation-play-state': 'paused',
+                '-webkit-animation-play-state': 'paused'
+            });
+            $('.song-pointer').addClass('song-pause');
+        });
+
+        $('.icon-play').on('click',function () {
+            audio.play();
+            $(this).removeClass('icon-show');
+            $('.icon-pause').addClass('icon-show');
+            $('.disc-img2').css({
+                'animation-play-state': 'running',
+                '-webkit-animation-play-state': 'running'
+            });
+            $('.disc-img3').css({
+                'animation-play-state': 'running',
+                '-webkit-animation-play-state': 'running'
+            });
+            $('.song-pointer').removeClass('song-pause');
+        });
+
+    });
     $.get('./lyric.json').then(function (object) {
-        let lyric = object.lyric;
+        let {lyric} = object;
         let lyricArr = lyric.split('\n');
         let regex = /^\[(.+)\](.*)$/;
         lyricArr = lyricArr.map(function (cur, idx) {
@@ -23,42 +67,5 @@ $(function () {
             $p.attr('data-time', 'cur.time').text(cur.words);
             $words.append($p);
         })
-    });
-
-    let audio = document.createElement('audio');
-    audio.src = '//dl.stream.qqmusic.qq.com/C400001Qu4I30eVFYb.m4a?fromtag=38&vkey=30B7E62CE7AAE2CBF403F5A432ACD0181DF43BCF64E73373B684400774C1C0BBF2CBD08C203BA703820F5D2E0FED16BF0834194F360B908B&guid=225426600';
-    audio.crossOrigin = "anonymous";
-    audio.oncanplay = function () {
-        audio.play();  //苹果出于保护用户的目的，禁止加载资源之后自动播放，巨坑。
-    };
-
-    $('.icon-pause').on('click',function () {
-        audio.pause();
-        $(this).removeClass('icon-show');
-        $('.icon-play').addClass('icon-show');
-        $('.disc-img2').css({
-            'animation-play-state': 'paused',
-            '-webkit-animation-play-state': 'paused'
-        });
-        $('.disc-img3').css({
-            'animation-play-state': 'paused',
-            '-webkit-animation-play-state': 'paused'
-        });
-        $('.song-pointer').addClass('song-pause');
-    });
-
-    $('.icon-play').on('click',function () {
-        audio.play();
-        $(this).removeClass('icon-show');
-        $('.icon-pause').addClass('icon-show');
-        $('.disc-img2').css({
-            'animation-play-state': 'running',
-            '-webkit-animation-play-state': 'running'
-        });
-        $('.disc-img3').css({
-            'animation-play-state': 'running',
-            '-webkit-animation-play-state': 'running'
-        });
-        $('.song-pointer').removeClass('song-pause');
     });
 });
